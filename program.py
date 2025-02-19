22710ba1ff06357d821f4f9209c8ce1016770614
DetAl12 <alchesin@packer.edu>
2025-02-19 09:34:35 -0500

    added 'Q' and 'R'

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R']
 
 for letter in letters:
     print(letter.lower())
