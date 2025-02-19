785fbf4edbfa92432ae0a232fb04748e02b8cbd7
DetAl12 <alchesin@packer.edu>
2025-02-19 09:35:09 -0500

    added 'S' and 'T'

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R']
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
 
 for letter in letters:
     print(letter.lower())
